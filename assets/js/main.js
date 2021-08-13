62e80e54f8c8fbfd8ca9e8488af32ae27210b7a7
Hung <manhhung14122@gmail.com>
2021-08-13 19:43:06 +0700

    reponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 var btnBuys = document.querySelectorAll('.js-buy-ticket');
 var modal = document.querySelector('.modal');
 var btnClose = document.querySelector('.modal-btn-close');
+var modalContainer = document.querySelector('.modal-container');
+// popup buy tickets modal
 for (item of btnBuys) {
     item.onclick = function () {
         modal.setAttribute('style', 'display:flex');
@@ -8,4 +10,46 @@ for (item of btnBuys) {
 }
 btnClose.onclick = function () {
     modal.setAttribute('style', 'display:none');
+}
+
+modal.onclick = function (e) {
+    modal.setAttribute('style', 'display:none');
+}
+modalContainer.addEventListener('click', function (e) {
+    e.stopPropagation();
+})
+
+
+// menu mobile 
+var header = document.getElementById('header');
+var btnMobile = document.querySelector('.mobile-menu-btn');
+var headerHeight = header.clientHeight;
+btnMobile.onclick = function () {
+    if (header.clientHeight === headerHeight) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+}
+// click menu 
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+var subnav = document.querySelector('.subnav');
+
+
+for (menuItem of menuItems) {
+    menuItem.onclick = function (e) {
+
+        var isParentMenu = this.nextElementSibling && 
+        this.nextElementSibling.classList.contains('subnav');
+        if (!isParentMenu) {
+            header.style.height = null;
+        } else {
+            e.preventDefault();
+            var isActive = subnav.classList.contains('active');
+            if (!isActive) {
+                subnav.classList.add('active');
+            } else
+                subnav.classList.remove('active');
+        }
+    }
 }
